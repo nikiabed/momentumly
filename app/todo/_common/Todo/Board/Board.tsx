27908c95da05173cd8372b66856d8b3667ecb290
nikiabed <nikiabedzade@gmail.com>
2026-06-11 13:22:46 +0330

    Add tags in each board

--- app/todo/_common/Todo/Board/Board.tsx
+++ app/todo/_common/Todo/Board/Board.tsx
@@ -8,17 +8,17 @@ import { Header } from "../../Header";
 import { gradiants, sidebar } from "../../Sidebar/Sidebar.const";
 import { Lists } from "../Lists";
 import { useMemo } from "react";
+import { titleToKey } from "../../Sidebar";
 
 export const Board = ({ item }: any) => {
-  const { todo, setTodo, boardList, searchText } = useTodoContext();
+  const { todo, setTodo, boardList } = useTodoContext();
   const currentBoard = item;
-  console.log("ITEM FULL:", item);
-  console.log("title:", item?.title);
-  console.log("boardKey:", item?.boardKey);
+
   const filteredTodos = todo.filter((t) => {
     if (!currentBoard) return false;
     if (currentBoard.boardKey === "all") return true;
     if (currentBoard.boardKey === "important") return t.isImportant;
+    if (currentBoard.boardKey === "complete") return t.status;
     return t.boardKey === currentBoard.boardKey;
   });
 
@@ -35,6 +35,9 @@ export const Board = ({ item }: any) => {
     }
   };
 
+  const activeTodos = filteredTodos.filter((t) => !t.status);
+  const completedTodos = filteredTodos.filter((t) => t.status);
+  console.log(completedTodos);
   return (
     <div
       className={` ${gradiants[currentBoard.color]} overflow-hidden flex-4 h-screen w-full py-5`}
@@ -42,23 +45,67 @@ export const Board = ({ item }: any) => {
       <div className="flex gap-4 flex-col h-screen py-5">
         <div className="shrink-0 px-15 flex flex-col gap-4 ">
           <Header item={currentBoard} />
-          {currentBoard?.title !== sidebar.progress && (
-            <TodoInput item={currentBoard} />
-          )}
+          {currentBoard?.boardKey !== "progress" &&
+            currentBoard?.boardKey !== "complete" && (
+              <TodoInput item={currentBoard} />
+            )}
         </div>
 
         {currentBoard?.title === "Progress" ? (
           <Progress item={currentBoard} />
         ) : (
           <div className=" overflow-y-auto grow flex flex-col gap-5 px-15 pb-5 w-full">
-            <TodoList todo={filteredTodos} setTodo={setTodo} />
-            {/* {currentBoard?.title === sidebar.myDay && completedTodo.length > 0 && (
-              <Lists
-                key={currentBoard?._id}
-                todo={completedTodo}
-                list={sidebar.complete}
-              />
-            )} */}
+            {/* MY DAY */}
+            {currentBoard.boardKey === "myDay" && (
+              <>
+                <TodoList todo={activeTodos} setTodo={setTodo} />
+                {completedTodos.length > 0 && (
+                  <Lists todo={completedTodos} list="انجام شده" />
+                )}
+              </>
+            )}
+
+            {currentBoard.boardKey === "all" && (
+              <>
+                {boardList?.map((board) => {
+                  const grouped = activeTodos.filter((t) => {
+                    return t.item === board.title;
+                  });
+
+                  if (!grouped.length) return null;
+
+                  return (
+                    <Lists key={board._id} todo={grouped} list={board.title} />
+                  );
+                })}
+
+                {completedTodos.length > 0 && (
+                  <Lists todo={completedTodos} list="انجام شده" />
+                )}
+              </>
+            )}
+
+            {currentBoard.boardKey === "complete" && (
+              <>
+                {boardList?.map((board) => {
+                  const grouped = completedTodos.filter((t) => {
+                    return t.item === board.title;
+                  });
+
+                  if (!grouped.length) return null;
+
+                  return (
+                    <Lists key={board._id} todo={grouped} list={board.title} />
+                  );
+                })}
+                
+              </>
+            )}
+
+            {!["myDay", "all", "complete"].includes(currentBoard.boardKey) && (
+              <TodoList todo={filteredTodos} setTodo={setTodo} />
+            )}
+
             {currentBoard?.title === sidebar.All &&
               boardList
                 ?.slice(sliceIndex())
