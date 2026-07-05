4fdee1cc2a4703e4baebd0f0b0a73977bdfe40d3
nikiabed <nikiabedzade@gmail.com>
2026-07-05 17:43:10 +0330

    Fixed redirect login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/todo");
+    }
+  }, [status]);
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-xl shadow-md text-center">
-        <h1 className="mb-4 text-xl font-bold">Welcome</h1>
+        <h1 className="mb-4 text-xl font-bold">خوش آمدید</h1>
 
         <div className="flex gap-3">
           <button
@@ -17,10 +27,8 @@ export default function LoginPage() {
             }
             className="px-4 py-2 bg-black text-white rounded-lg cursor-pointer"
           >
-            Continue with Google
+            ادامه با گوگل
           </button>
-
-          
         </div>
       </div>
     </div>
