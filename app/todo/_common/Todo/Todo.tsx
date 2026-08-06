dac62f62e9d289e2e4b4ba216f972267b7470870
nikiabed <nikiabedzade@gmail.com>
2026-08-06 12:05:30 +0330

    Add todo loader and skeleton

--- app/todo/_common/Todo/Todo.tsx
+++ app/todo/_common/Todo/Todo.tsx
@@ -23,7 +23,26 @@ export const Todo = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   }, [activeBoard, searchText, uiBoard, systemBoards]);
 
   if (!uiBoard?.length) {
-    return <div>Loading boards...</div>;
+    return (
+      <div className="flex flex-col gap-8 px-16 py-5 animate-pulse">
+        <div className="space-y-3">
+          <div className="h-8 w-56 rounded-lg bg-foreground/10" />
+          <div className="h-4 w-32 rounded bg-foreground/10" />
+        </div>
+        <div className="h-11 rounded-xl bg-foreground/10" />
+        <div className="space-y-4">
+          {Array.from({ length: 7 }).map((_, i) => (
+            <div
+              key={i}
+              className="flex items-center gap-4 rounded-xl border border-border-gray p-4"
+            >
+              <div className="h-5 w-5 rounded-full bg-foreground/10" />
+              <div className="h-4 flex-1 rounded bg-foreground/10" />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 
   return (
