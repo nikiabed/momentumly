1ac941e27e6765102a86ee1f8a9ffa67b5cf7d3e
nikiabed <nikiabedzade@gmail.com>
2026-08-07 18:12:06 +0330

    Fixed focusMinutes XPstates

--- app/_utils/progress/getXPStats.ts
+++ app/_utils/progress/getXPStats.ts
@@ -1,13 +1,30 @@
 import { TodoList } from "@/app/types";
 import { calculateXP } from "./calculateXP";
+import { getCompletionType } from "./completion";
+import { isPlannedForDay } from "./todoAnalytics";
 
 export function getXPStats(todos: TodoList) {
-  const completedTodos = todos.filter((t) => t.status && t.completedAt);
-
-  const globalXP = completedTodos.reduce(
-    (sum, todo) => sum + calculateXP(todo),
-    0,
+  const completedTodos = todos.filter(
+    (todo) => todo.status && todo.completedAt,
   );
+
+  const globalXP = completedTodos.reduce((sum, todo) => {
+    const planned = isPlannedForDay(todo, new Date(todo.completedAt!)) ? 1 : 0;
+
+    const onTime = getCompletionType(todo) === "onTime" ? 1 : 0;
+
+    const recovered = getCompletionType(todo) === "delayed" ? 1 : 0;
+
+    const xp = calculateXP({
+      completed: 1,
+      planned,
+      onTime,
+      recovered,
+      focusMinutes: 0,
+    });
+
+    return sum + xp;
+  }, 0);
 
   return {
     globalXP,
